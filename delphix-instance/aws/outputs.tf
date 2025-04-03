69e6d096afcc09b717a69df120c1db13dadcfe51
Sumeet Singh <singhsumeet361@gmail.com>
2025-04-03 12:59:46 +0100

    Update outputs.tf

--- delphix-instance/aws/outputs.tf
+++ delphix-instance/aws/outputs.tf
@@ -9,3 +9,8 @@ output "instance_id" {
 output "instance_ip" {
   value = aws_instance.instance.private_ip
 }
+
+output "instance_dns_name" {
+  value = var.hosted_zone != "" ? "${var.instance_name}.${var.hosted_zone}" : ""
+  description = "The DNS name of the instance in Route 53"
+}
